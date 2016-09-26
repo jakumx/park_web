685f895ed881fe6869ce678813a9f32d3d5f79b1
josuemx <artu_ja@hotmail.com>
2016-09-26 14:05:10 -0500

    headers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,6 +41,11 @@ angular
     $httpProvider.defaults.useXDomain = true;
     $httpProvider.defaults.withCredentials = true;
     delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $http.defaults.headers.put = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
+        };
     $httpProvider.defaults.headers.common["Accept"] = "application/json";
     $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 
