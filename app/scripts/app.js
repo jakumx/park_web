35b1f2673e2652b228592b347610f80472b1652f
josuemx <artu_ja@hotmail.com>
2016-09-26 13:50:10 -0500

    headers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -37,7 +37,12 @@ angular
   })
   .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.withCredentials = true;
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 
     $stateProvider
       .state('app', {
